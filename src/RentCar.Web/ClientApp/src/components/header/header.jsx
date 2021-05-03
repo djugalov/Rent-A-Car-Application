26464d8d59a0d653e59cad1a4f46a0e7acb03e9f
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-05-04 00:40:29 +0300

    Render nav items based on user logged in status

--- src/RentCar.Web/ClientApp/src/components/header/header.jsx
+++ src/RentCar.Web/ClientApp/src/components/header/header.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
+import { inject, observer } from 'mobx-react';
 
-export const Header = () => {
+export const Header = inject('RootStore')(observer(({ RootStore }) => {
 
     const [activeItem, setActiveItem] = useState("home");
 
@@ -9,34 +10,48 @@ export const Header = () => {
 
     const handleAuthClick = (name) => {
         setActiveItem(name);
-        window.location.pathname = name==='signUp' ? '/identity/account/register': '/identity/account/login';
+        window.location.pathname = name === 'signUp' ? '/identity/account/register' : '/identity/account/login';
     }
+
+    const renderNavItemsBasedOnUserAuth = () => {
+        console.log(RootStore.userStore.isUserAuthenticated)
+        return RootStore.userStore.isUserAuthenticated ?
+            <Menu.Item
+                name='My Account'
+                active={activeItem === 'myAccount'}
+                onClick={() => handleItemClick("myAccount")}
+            /> :
+            <>
+                <Menu.Item
+                    name='Sign In'
+                    active={activeItem === 'signIn'}
+                    onClick={() => handleAuthClick("signIn")}
+                />
+                <Menu.Item
+                    name='Sign Up'
+                    active={activeItem === 'signUp'}
+                    onClick={() => handleAuthClick("signUp")}
+                />
+            </>
+    };
+
     return (
-            <Segment inverted>
-                <Menu inverted pointing secondary>
-                    <Menu.Item
-                        name='home'
-                        active={activeItem === 'home'}
-                        onClick={() => handleItemClick("home")}
-                    />
-                    <Menu.Item
-                        name='Book a Car'
-                        active={activeItem === 'book'}
-                        onClick={() => handleItemClick("book")}
-                    />
-                    <Menu.Menu position='right'>
-                        <Menu.Item
-                            name='Sign In'
-                            active={activeItem === 'signIn'}
-                            onClick={() => handleAuthClick("signIn")}
-                        />
-                        <Menu.Item
-                            name='Sign Up'
-                            active={activeItem === 'signUp'}
-                            onClick={() => handleAuthClick("signUp")}
-                        />
-                    </Menu.Menu>
-                </Menu>
-            </Segment>
+        <Segment inverted>
+            <Menu inverted pointing secondary>
+                <Menu.Item
+                    name='home'
+                    active={activeItem === 'home'}
+                    onClick={() => handleItemClick("home")}
+                />
+                <Menu.Item
+                    name='Book a Car'
+                    active={activeItem === 'book'}
+                    onClick={() => handleItemClick("book")}
+                />
+                <Menu.Menu position='right'>
+                    {renderNavItemsBasedOnUserAuth()}
+                </Menu.Menu>
+            </Menu>
+        </Segment>
     )
-}
+}))
