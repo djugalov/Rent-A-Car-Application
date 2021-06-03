cac29702eb4f22e23a85e19fa78509373dea82bc
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-06-03 13:10:46 +0300

    Extract common logic for date time formatting, discount calcs and input state validation in helper

--- src/RentCar.Web/ClientApp/src/utils/helpers/helpers.js
+++ src/RentCar.Web/ClientApp/src/utils/helpers/helpers.js
@@ -1,3 +1,5 @@
+import Constants from "./constants";
+
 const Helpers = (() => {
 
     const formatTime = (dateString) => {
@@ -5,8 +7,42 @@ const Helpers = (() => {
         return (date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear());
     }
 
+    const getDiscountPercentage = (daysRented, isVip) => {
+        if (isVip) {
+            return Constants.VipLevelPercentageDiscount;
+        }
+        if (daysRented > 3) {
+            return Constants.BronzeLevelPercentageDiscount;
+        }
+        if (daysRented > 5) {
+            return Constants.SilverLevelPercentageDiscount;
+        }
+        if (daysRented > 10) {
+            return Constants.GoldLevelPercentageDiscount;
+        }
+        return 0;
+    }
+
+    const calculateTotalDays = (startTime, endTime) => {
+        let diffInTime = endTime.getTime() - startTime.getTime();
+        return diffInTime / (1000 * 3600 * 24);
+    }
+
+    const isStateValid = (dto) => {
+        let isValid = true;
+        Object.values(dto).map(x => {
+            if (x === undefined || x === "" || x === null) {
+                isValid = false;
+            }
+        })
+        return isValid;
+    }
+
     return {
-        formatTime
+        formatTime,
+        getDiscountPercentage,
+        calculateTotalDays,
+        isStateValid
     }
 })();
 
