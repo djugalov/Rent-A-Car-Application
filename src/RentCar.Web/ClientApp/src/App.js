0b9b7cbba78d6bb3c0ff4ef601acf6396a251483
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-06-03 13:16:44 +0300

    Remove unused import

--- src/RentCar.Web/ClientApp/src/App.js
+++ src/RentCar.Web/ClientApp/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import './custom.css'
