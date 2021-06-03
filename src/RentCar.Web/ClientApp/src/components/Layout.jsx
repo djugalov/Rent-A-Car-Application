8cc80dc4565bdffde1fc780b8742545919a80b33
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-06-03 13:14:48 +0300

    Build routes based on user level of access

--- src/RentCar.Web/ClientApp/src/components/Layout.jsx
+++ src/RentCar.Web/ClientApp/src/components/Layout.jsx
@@ -4,18 +4,37 @@ import { Header } from './header/header';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import VehiclesList from './vehiclesList/vehiclesList';
+import VehicleManagementList from './vehicleManagement/vehicleManagementList';
+import UserManagementList from './userManagement/userManagementList';
+import MyAccount from './myAccount/myAccount';
+import ExpiredRentalEventsList from './expiredRentalEvents/expiredRentalEventsList';
+import Home from './home/home';
+import { Message } from './message/message';
 
 const Layout = inject('RootStore')(observer(({ RootStore }) => {
   useEffect(() => {
     RootStore.userStore.checkIfUserIsAuthenticated();
-    console.log(RootStore.userStore.isUserAuthenticated)
+    RootStore.userStore.getCurrentUser();
   }, [RootStore.userStore])
+
+  const renderAdminRoutes = () => RootStore.userStore.currentUser.isAdmin ? <Switch>
+    <Route path='/management' component={() => <VehicleManagementList />} />
+    <Route path='/userManagement' component={() => <UserManagementList />} />
+    <Route path='/rentalEventsManagement' component={() => <ExpiredRentalEventsList />} />
+  </Switch> : <></>
 
   return (
     <div>
-      <Header />
+      <Message/>
       <BrowserRouter>
-          <Route path='/book' component={() => <VehiclesList/>} />
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <Home />} />
+          <Route path='/home' component={() => <Home />} />
+          <Route path='/book' component={() => <VehiclesList />} />
+          <Route path='/myAccount' component={() => <MyAccount />} />
+          {renderAdminRoutes()}
+        </Switch>
       </BrowserRouter>
     </div>
   );
