fb8c5a0d02ed7c13f3dfe63c6aa7bac77fd77505
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-06-03 13:12:56 +0300

    configure all get and post requests used

--- src/RentCar.Web/ClientApp/src/ajax/ajax.js
+++ src/RentCar.Web/ClientApp/src/ajax/ajax.js
@@ -12,11 +12,47 @@ const Ajax = (()=>{
 
     const isUserLoggedIn = (onSuccess, onError) => get(Urls.isUserLoggedIn, onSuccess, onError);
 
+    const getCurrentUser = (onSuccess, onError) => get(Urls.getCurrentUser, onSuccess, onError);
+
+    const getAllUsers = (onSuccess, onError) => get(Urls.getAllUsers, onSuccess, onError);
+
+    const getUserByID = (onSuccess, onError) => get(Urls.getUserByID, onSuccess, onError);
+
+    const editUser = (data, onSuccess, onError) => post(Urls.editUser, data, onSuccess, onError);
+
+    const deleteUser = (id, onSuccess, onError) => post(Urls.deleteUser, id, onSuccess, onError);
+
     const getAllVehicles = (onSuccess, onError) => get(Urls.getAllVehicles, onSuccess, onError);
+
+    const getAllAvailableVehicles = (onSuccess, onError) => get(Urls.getAllAvailableVehicles, onSuccess, onError);
+
+    const bookVehicle = (data, onSuccess, onError) => post(Urls.bookVehicle, data, onSuccess, onError); 
+
+    const deleteVehicle = (id, onSuccess, onError) => post(Urls.deleteVehicle, id, onSuccess, onError);
+
+    const addVehicle = (data, onSuccess, onError) => post(Urls.addVehicle, data, onSuccess, onError);
+
+    const editVehicle = (data, onSuccess, onError) => post(Urls.editVehicle, data, onSuccess, onError);
+
+    const getExpiredRentalEvents = (onSuccess, onError) => get(Urls.getExpiredRentalEvents, onSuccess, onError);
+
+    const markAsAvailable = (data, onSuccess, onError) => post(Urls.markAsAvailable, data, onSuccess, onError);
     
     return{
         isUserLoggedIn,
-        getAllVehicles
+        getCurrentUser,
+        getAllVehicles,
+        bookVehicle,
+        getAllAvailableVehicles,
+        deleteVehicle,
+        addVehicle,
+        editVehicle,
+        getAllUsers,
+        editUser,
+        deleteUser,
+        getUserByID,
+        getExpiredRentalEvents,
+        markAsAvailable
     }
 })();
 
