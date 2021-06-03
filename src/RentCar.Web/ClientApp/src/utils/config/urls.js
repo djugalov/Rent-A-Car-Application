fe9cba3ca8ac1a9e99d0c5b8cb5f812182364e0f
Georgi Dzhugalov <georgi.djugalov@gmail.com>
2021-06-03 13:12:32 +0300

    Add urls to exisiting endpoint

--- src/RentCar.Web/ClientApp/src/utils/config/urls.js
+++ src/RentCar.Web/ClientApp/src/utils/config/urls.js
@@ -1,13 +1,50 @@
 const Urls = (()=>{
+
     const baseUrl = 'https://localhost:5001/';
 
     const isUserLoggedIn = `${baseUrl}api/user/IsUserLoggedIn`;
 
+    const getCurrentUser = `${baseUrl}api/user/GetCurrentUser`;
+
+    const getAllUsers = `${baseUrl}api/user/getAllUsers`;
+
+    const getUserByID = `${baseUrl}api/user/getUserByID`;
+
+    const editUser = `${baseUrl}api/user/editUser`;
+
+    const deleteUser = `${baseUrl}api/user/deleteUser`;
+
     const getAllVehicles = `${baseUrl}api/vehicle/getAllVehicles`;
+
+    const bookVehicle = `${baseUrl}api/vehicle/bookVehicle`;
+
+    const getAllAvailableVehicles = `${baseUrl}api/vehicle/getAllAvailableVehicles`;
+
+    const deleteVehicle = `${baseUrl}api/vehicle/deleteVehicle`;
+
+    const addVehicle = `${baseUrl}api/vehicle/addVehicle`;
+
+    const editVehicle = `${baseUrl}api/vehicle/editVehicle`;
+
+    const getExpiredRentalEvents = `${baseUrl}api/rentalEvent/getExpiredRentalEvents`;
+
+    const markAsAvailable = `${baseUrl}api/vehicle/markAsAvailable`;
 
     return{
         isUserLoggedIn,
-        getAllVehicles
+        getCurrentUser,
+        getAllUsers,
+        getAllVehicles,
+        bookVehicle,
+        getAllAvailableVehicles,
+        deleteVehicle,
+        addVehicle,
+        editVehicle,
+        deleteUser,
+        getUserByID,
+        editUser,
+        getExpiredRentalEvents,
+        markAsAvailable
     }
 })();
 
